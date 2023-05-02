e40992f12d2f109fcae5e35394f4eb5412779173
Pogorelov Yura <pogorelov.yurik@gmail.com>
2023-05-02 22:52:19 +0300

    Player Controller

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -14,6 +14,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        HandleMovementInput();
+        HandleRotationInput();
+    }
+
+    void HandleMovementInput()
+    {
+        float _horizontal = Input.GetAxis("Horizontal");
+        float _vertical = Input.GetAxis("Vertical");
+
+        Vector3 _movement = new Vector3(_horizontal, 0, _vertical);
+        transform.Translate(_movement * movementSpeed * Time.deltaTime, Space.World);
+    }
+
+    void HandleRotationInput()
+    {
+        RaycastHit _hit;
+        Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(_ray, out _hit))
+        {
+            transform.LookAt(new Vector3(_hit.point.x, transform.position.y, _hit.point.z));
+        }
     }
 }
