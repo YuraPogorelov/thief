d3c0f76d3543cde6a8f1fa1b76fd47be027a59e6
Pogorelov Yura <pogorelov.yurik@gmail.com>
2023-05-02 21:31:57 +0300

    Правки

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,25 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float _movemenSpeed = 10f;
-
-    private Rigidbody _rb;
-    // Start is called before the first frame update
+    [SerializeField] private float movementSpeed;
+    
     void Start()
     {
-        _rb = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 mov = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")) * _movemenSpeed;
-        mov = Vector3.ClampMagnitude(mov, _movemenSpeed);
-
-        if (mov != Vector3.zero)
-        {
-            _rb.MovePosition(transform.position + mov * Time.deltaTime);
-            _rb.MoveRotation(Quaternion.LookRotation(mov));
-        }
+        
     }
 }
